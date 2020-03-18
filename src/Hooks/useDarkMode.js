e984a3dc68565b6d4335ce0d7991bc06f44620b3
Jose <pjose14.jp38@yahoo.com>
2020-03-17 22:01:05 -0500

    dark mode only works afer refreshing the page

--- src/Hooks/useDarkMode.js
+++ src/Hooks/useDarkMode.js
@@ -1,15 +1,17 @@
 import { useEffect } from "react";
 import { useLocalStorage } from "../useLocalStorage/useLocalStorage.js";
 
-export const UseDarkMode = (key, initialValues, callback) => {
-    const [darkMode, setDarkMode ] = 
+export const useDarkMode = (key, initialValues, callback) => {
+    const [darkMode, setDarkMode ] = useLocalStorage (key, initialValues);
 
     useEffect(() => {
         if(darkMode) {
-
+            document.body.classList.add('dark-mode')
         } else {
-
+            document.body.classList.remove('dark-mode')
         }
-    })
+    }, [darkMode])
+    return [darkMode, setDarkMode];
 }
 
+export default useDarkMode;
--- src/useLocalStorage/useLocalStorage.js
+++ src/useLocalStorage/useLocalStorage.js
@@ -1,5 +1,12 @@
-import { useStat } from "react";
+import { useState } from "react";
 
 export const useLocalStorage = (key, initialValue) => {
+    const item = JSON.parse(localStorage.getItem(key));
 
+    const [value, setStoredValue] = useState(item || initialValue);
+
+    const setValue = (value) => {
+        window.localStorage.setItem(key, JSON.stringify(value));
+    }
+    return [value, setValue];
 }
